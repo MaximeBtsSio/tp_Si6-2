683448c8da23e1b762d36e6e3952b174dea5f3d1
Maxime <maxime.saliou@bts-malraux.net>
2015-01-21 12:02:45 +0100

    Premier truc

---
+++ main.c
@@ -0,0 +1,15 @@
+int main
+{
+double operateurA, operateurB, resultat;
+
+printf("Saisir A");
+scanf(operateurA);
+printf("Saisir B");
+scanf(operateurB);
+
+resultat=operateurA*operateurB
+printf("le resultat de la multiplication est %d ",resultat);
+
+resultat= operateurA/operateurB
+printf("le resultat de la division est %d", resultat);
+}
