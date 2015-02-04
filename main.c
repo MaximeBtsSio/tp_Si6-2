dc8b3f947febdb471fa2171d1689a433c5ea8440
MaximeSaliou <maxime.saliou@bts-malraux.net>
2015-02-04 11:52:19 +0100

    Update main.c
    Ajout du Switch

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 int main
 {
 double operateurA, operateurB, resultat+, resultat-,resultat*,resultat/;
+string Operation;
+
 printf("Veuillez choisir le type de calcul que vous souhaitez executer");
 
 
@@ -10,37 +12,57 @@ scanf(operateurA);
 printf("Saisir B");
 scanf(operateurB);
 
-
+//déclaration de la fonction Soustraire
 double add(double A,B)
 {
 return A-B;
 }
-
+//déclaration de la fonction Additioner
 double add(double A,B)
 {
   return A+B
 }
-
+//déclaration de la fonction Multiplier
 double add(double A,B)
 {
   return A*B;
 }
-
+//déclaration de la fonction Diviser
 double add(double A,B)
 {
   return A/B;
 }
-
+//résultat de la fontion Soustraire
 resultat-=add(A,B)
 printf (resultat-);
-
+//résultat de la fontion Additioner
 resultat+=add(A,B)
 printf (resultat+);
-
+//résultat de la fontion Multiplier
 resultat*=add(A,B)
 printf (resultat*);
-
+//résultat de la fontion Diviser
 resultat/=add(A,B)
 printf (resultat/);
+
+switch (operation)
+{
+  case operation = "Multiplication"
+  A*B;
+  break;
+  case operation = "Division"
+  A/B;
+  break;
+  case operation = "Soustraction"
+  A-B;
+  break;
+  case operation = "Addition"
+  A+B;
+  break;
+  
+  default
+  printf("Erreur");
+  break;
+}
 
 }
